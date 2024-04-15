9816debd6d26ebc28827aa9158900677624402ad
Teresiah-cpu <terrywambo2018@gmail.com>
2024-04-15 23:59:06 +0300

    created a while loop

---
+++ while_loop.js
@@ -0,0 +1,22 @@
+/*let i = 2;
+
+
+
+while (i < 6) {
+
+console.log(`The value of i = ${i}`);
+
+i++;
+
+} */
+
+//Because the value of i never changes, the while loop will go on forever!
+/*let i = 0;
+
+
+
+while (i < 6) {
+
+console.log(`The value of i = ${i}`);
+
+}*/
