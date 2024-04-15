795908343d5ba928086f0480e596800e4e3d1d3c
Teresiah-cpu <terrywambo2018@gmail.com>
2024-04-15 23:23:30 +0300

    created files for exercises on use of switch cases

---
+++ exercise.js
@@ -0,0 +1,15 @@
+// Exercise #5
+
+// Define the grade variable to store the student's grade
+let grade = "A";
+
+// Check the grade and give appropriate rewards
+if (grade === "A") {
+  console.log("You got an A, so here's a Chocolate for you!");
+} else if (grade === "B") {
+  console.log("You got a B, here's a Cookie for you!");
+} else if (grade === "C") {
+  console.log("You got a C, there's room for improvement and here's your Candy!");
+} else {
+  console.log("No reward to give.");
+}
---
+++ switch_exercise1.js
@@ -0,0 +1,19 @@
+// Exercise #5
+
+// Define the grade variable to store the student's grade
+let grade = "C";
+
+// Check the grade and give appropriate rewards
+switch (grade) {
+  case "A":
+    console.log("You got an A, so here's a Chocolate for you!");
+    break;
+  case "B":
+    console.log("You got a B, here's a Cookie for you!");
+    break;
+  case "C":
+    console.log("You got a C, there's room for improvement and here's your Candy!");
+    break;
+  default:
+    console.log("No reward to give.");
+}
